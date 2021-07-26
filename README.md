3f44d8d9a0f7cadbbdb17b4e9fd016b4c48d9a83
danil271 <daniltanjung039@gmail.com>
2021-07-26 18:28:00 +0700

    add file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# uas_iuran_kas_rt_PW
+# uas-pemweb
+# Alfan Maulana
+# 311910643
+# TI 19 B2
+
+# TAMPILAN INDEX
+![SS 1](https://user-images.githubusercontent.com/81576195/126422554-548aedd5-b26c-474a-be17-c521417c231e.jpg)
+
+# TAMBAH DATA
+![SS 2](https://user-images.githubusercontent.com/81576195/126423647-10e45629-eb4f-4c25-81f2-7306f59e96a0.jpg)
+![SS 3](https://user-images.githubusercontent.com/81576195/126424806-6900b0ce-b987-4fa5-9c2c-f68985cd5ede.jpg)
+![SS 4](https://user-images.githubusercontent.com/81576195/126424812-a37f12af-6c2b-437d-9da5-6f439d79f65c.jpg)
+
+# UBAH DATA
+![SS 5](https://user-images.githubusercontent.com/81576195/126425871-f8048c44-c511-4ba6-a635-37972386e3d7.jpg)
+
+# HAPUS DATA
+![SS 6](https://user-images.githubusercontent.com/81576195/126427364-5389d7c1-0a36-417c-9a50-f9b8c20afe9f.jpg)
+![SS 7](https://user-images.githubusercontent.com/81576195/126427383-31f643b5-ff90-4fbf-8120-ab049b325320.jpg)
+
---
+++ pos_db.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2021 at 07:09 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Iuran Kas RT'),
+(2, 'Ronda'),
+(3, 'Arisan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) DEFAULT NULL,
+  `product_price` decimal(11,0) DEFAULT NULL,
+  `product_category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `product_category_id`) VALUES
+(1, 'Alfan', '20000', 1),
+(2, 'Jannah', '40000', 1),
+(3, 'Resia', '60000', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
